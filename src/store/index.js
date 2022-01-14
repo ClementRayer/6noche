9821aa9bd424d576158e75494127263ff9649568
ClementRayer <clement.rayer@gmail.com>
2022-01-14 16:56:47 +0100

    Content update 14/01

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,110 @@ export default createStore({
   state: {
     movies: [
       {
+        title: 'Old Boy',
+        note: 5,
+        director: 'Park Chan-wook',
+        genre: ['Action', 'Thriller'],
+        cast: ['Choi Min-sik', 'Yu Ji-tae', 'Kang Hye-jeong'],
+        release: {
+          year: 2003,
+          month: 11
+        },
+        viewDate: {
+          year: 2022,
+          month: 1,
+          day: 13
+        },
+        coverPath: 'old-boy.jpg',
+        isBanger: true
+      },
+      {
+        title: 'Battle of the Sexes',
+        note: 4,
+        director: 'Dayton / Faris',
+        genre: ['Biopic', 'Comédie'],
+        cast: ['Emma Stone', 'Steve Carell', 'Andrea Riseborough'],
+        release: {
+          year: 2017,
+          month: 9
+        },
+        viewDate: {
+          year: 2022,
+          month: 1,
+          day: 12
+        },
+        coverPath: 'battle-of-the-sexes.jpg'
+      },
+      {
+        title: 'Divergent',
+        note: 1,
+        director: 'Neil Burger',
+        genre: ['Science-Fiction'],
+        cast: ['Shailene Woodley', 'Theo James', 'Kate Winslet'],
+        release: {
+          year: 2014,
+          month: 3
+        },
+        viewDate: {
+          year: 2022,
+          month: 1,
+          day: 11
+        },
+        coverPath: 'divergent.jpg'
+      },
+      {
+        title: 'Three Billboards Outside Ebbing, Missouri',
+        note: 5,
+        director: 'Martin McDonagh',
+        genre: ['Drame'],
+        cast: ['Frances McDormand', 'Woody Harrelson', 'Sam Rockwell'],
+        release: {
+          year: 2017,
+          month: 11
+        },
+        viewDate: {
+          year: 2022,
+          month: 1,
+          day: 9
+        },
+        coverPath: 'three-billboards.jpg',
+        isBanger: true
+      },
+      {
+        title: 'Under The Silver Lake',
+        note: 4,
+        director: 'David Robert Mitchell',
+        genre: ['Thriller', 'Noir'],
+        cast: ['Andrew Garfield', 'Riley Keough'],
+        release: {
+          year: 2018,
+          month: 4
+        },
+        viewDate: {
+          year: 2022,
+          month: 1,
+          day: 8
+        },
+        coverPath: 'under-the-silver-lake.jpg'
+      },
+      {
+        title: 'The Green Knight',
+        note: 5,
+        director: 'David Lowery',
+        genre: ['Fantastique', 'Historique'],
+        cast: ['Dev Patel', 'Alicia Vikander'],
+        release: {
+          year: 2021,
+          month: 7
+        },
+        viewDate: {
+          year: 2022,
+          month: 1,
+          day: 7
+        },
+        coverPath: 'the-green-knight.jpg'
+      },
+      {
         title: 'The Favourite',
         note: 4,
         director: 'Yórgos Lánthimos',
@@ -106,6 +210,16 @@ export default createStore({
           day: 1
         },
         coverPath: 'crazy-stupid-love.jpg'
+      }
+    ],
+    observations: [
+      {
+        date:{
+          month: 1,
+          day: 8
+        },
+        imagePath: 'into the spider-verse "great expecatations',
+        content: "Après une semaine complète, mon sentiment est mitigé, mais globablement positif. Sans m'être réellement fixé d'objectif, l'idée originelle était de voir un film par jour, sans exception, durant toute l'année. Après avoir vu 7 films, dont 6 devait durer plus de deux heures, je remet sérieusement en doute cet objectif qui paraît bien plus lourd que prévu pour une telle durée. Ainsi, je pense donc viser plutôt 300 films vus dans l'année, ce qui me permettra de 'libérer' deux jours par semaine, ou d'avoir un matelas de sécurité. Si je prend pleinement conscience de la contrainte, je suis cependant ravi de me pousser à regarder des films, en évitant au maximum les revisionnages: j'ai découvert des films que j'aime beaucoup, dont un qui m'a fait une très forte impression et que je pense garder haut dans mon estime pour une longue période. Malgré le temps libre dont je dispose, j'en suis cependant déjà à chercher des films courts quand le choix est uniquement mien. Invariablement, je me suis jusqu'ici retrouvé chaque jour à regarder un film long mais dont je ne voyais pas passer les minutes, absorbé par leur qualité. Je vais peut-être m'habituer au rythme et ne pas avoir tant de difficulté que cela, mais j'appréhende sérieusement le moment où je ne serai pas conquis par plusieurs films de suite, j'espère ne pas flancher et tenir bon. Great expectations."
       }
     ]
   },
